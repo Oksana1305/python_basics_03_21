72bc4fcc7c27ffe92cf27a225eb28393af03c0d9
swaldom <svichkar.dev@gmail.com>
2021-03-10 22:25:41 +0200

    lesson 1: upd practice (solutions)

--- lesson1/_5_practice.py
+++ lesson1/_5_practice.py
@@ -7,3 +7,30 @@
     5. Измените значение переменной var_str на 'end'
     6. Выведите значения всех переменных
 """
+
+# 1
+var_int = 25
+var_float = 4.5
+var_str = "python"
+
+# вариант с множественным присваиванием
+var_int, var_float, var_str = 25, 4.5, "python"
+
+# 2
+var_big = var_int * 1.5
+
+# 3
+var_float = var_float - 1
+
+# с использованием оператора присваивания
+# var_float -= 1
+
+# 4
+result = var_big % var_float
+print(result)
+
+# 5
+var_str = "end"
+
+# 6
+print(var_big, var_float, var_str)
